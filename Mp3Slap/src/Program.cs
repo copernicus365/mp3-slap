7b9aa55bdc06164b3c74b92f3227bbe7c5a1a44c
Nicholas Petersen <copernicus365@gmail.com>
2024-11-13 02:14:05 -0500

    -

--- Mp3Slap/src/Program.cs
+++ Mp3Slap/src/Program.cs
@@ -5,45 +5,36 @@ using static System.Console;
 WriteLine("mp3 splitter!");
 
 string dir = "C:/Dropbox/Vids/mp3-split/mp3-split/test1";
+dir = "C:/Dropbox/Music/Bible/Suchet-NIV-1Album";
 
-double silenceLen = 2.2;
+bool writeRaw = false;
+//ConsoleKey ky = ReadKey().Key;
+//bool writeRaw = ky == ConsoleKey.D1 || ky == ConsoleKey.T;
 
-string logsNm = $"logs1-{silenceLen}s";
+double[] silenceLens = [2.0, 2.3, 2.5, 2.7, 3.0, 3.3, 3.7];
 
-"Enter '1' or 't' to generate raw script that can be ran in bash to generate the raw ffmpeg silence files. Else anything else to PARSE them into csvs.".Print();
+for(int i = 0; i < silenceLens.Length; i++) {
+	double silenceLen = silenceLens[i];
 
-ConsoleKey ky = Console.ReadKey().Key;
+	string logsNm = $"logs1-{silenceLen}s";
 
-bool writeRaw = ky == ConsoleKey.D1 || ky == ConsoleKey.T;
+	"Enter '1' or 't' to generate raw script that can be ran in bash to generate the raw ffmpeg silence files. Else anything else to PARSE them into csvs.".Print();
 
-if(writeRaw) {
-	AlbumToTracksInfo ati = new(dir) {
-		LogFolderName = logsNm,
-		RemoveRootDirFromScript = true,
-	};
+	if(writeRaw) {
+		AlbumToTracksInfo ati = new(dir) {
+			LogFolderName = logsNm,
+			RemoveRootDirFromScript = true,
+		};
 
-	await ati.RUN(silenceInSecs: silenceLen, runProcess: false);
+		await ati.RUN(silenceInSecs: silenceLen, runProcess: false);
 
-	string script = ati.Scripts;
+		string script = ati.Scripts;
 
-	WriteLine("end mp3 splitter!");
-
+		WriteLine("end mp3 splitter!");
+	}
+	else {
+		// write csvs from raw results
+		RunParserOnLogsFolder ff = new();
+		ff.RUN(logsDir: dir + '/' + logsNm);
+	}
 }
-else {
-	// write csvs from raw results
-
-	RunParserOnLogsFolder ff = new();
-	ff.RUN(logsDir: dir + '/' + logsNm);
-}
-
-
-return;
-
-
-
-
-
-
-
-//string dir = "/Users/nikos/repos/praxis/mp3-split/aud/";
-//dir = "/Users/nikos/repos/praxis/mp3-split/messiah";
