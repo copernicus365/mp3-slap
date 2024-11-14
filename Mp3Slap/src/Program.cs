dcc3bf9bb4846327499cf5759cb5bb9c8e65b896
Nicholas Petersen <copernicus365@gmail.com>
2024-11-14 03:42:05 -0500

    big prog

--- Mp3Slap/src/Program.cs
+++ Mp3Slap/src/Program.cs
@@ -1,40 +1,115 @@
-using Mp3Slap;
-
 using static System.Console;
 
-WriteLine("mp3 splitter!");
+namespace Mp3Slap;
 
-string dir = "C:/Dropbox/Vids/mp3-split/mp3-split/test1";
-dir = "C:/Dropbox/Music/Bible/Suchet-NIV-1Album";
+public class Runner
+{
+	public static async Task Main(string[] args)
+		=> await new Runner().RUN(args);
 
-bool writeRaw = false;
-//ConsoleKey ky = ReadKey().Key;
-//bool writeRaw = ky == ConsoleKey.D1 || ky == ConsoleKey.T;
+	static string TestPath = "C:/Dropbox/Music/Bible/Suchet-NIV-1Album";
+	// "C:/Dropbox/Vids/mp3-split/mp3-split/test1"
 
-double[] silenceLens = [2.0, 2.3, 2.5, 2.7, 3.0, 3.3, 3.7];
+	public async Task RUN(string[] args)
+	{
+		if(args != null)
+			args = args.Select(v => v.NullIfEmptyTrimmed()).Where(v => v != null).ToArray();
 
-for(int i = 0; i < silenceLens.Length; i++) {
-	double silenceLen = silenceLens[i];
+		WriteLine("mp3 slap!");
 
-	string logsNm = $"logs1-{silenceLen}s";
+		bool elseUseDef = true;
+		if(elseUseDef && args.IsNulle()) {
+			args = ["C:/Dropbox/Music/Bible/Suchet-NIV-1Album"];
+		}
 
-	"Enter '1' or 't' to generate raw script that can be ran in bash to generate the raw ffmpeg silence files. Else anything else to PARSE them into csvs.".Print();
+		RunnerSettings stg = null;
 
-	if(writeRaw) {
-		AlbumToTracksInfo ati = new(dir) {
-			LogFolderName = logsNm,
-			RemoveRootDirFromScript = true,
-		};
+		GetSettingsPath getStgsP = new();
+		if(!getStgsP.RUN(args?.FirstOrDefault())) {
+			WriteLine(getStgsP.Error);
+			return;
+		}
 
-		await ati.RUN(silenceInSecs: silenceLen, runProcess: false);
+		if(stg == null) {
+			stg = RunnerSettings.TryParse(getStgsP.SettingsContent, getStgsP.RootDirectory);
+			if(stg == null) {
+				WriteLine("Invalid settings");
+				return;
+			}
+		}
 
-		string script = ati.Scripts;
+		double[] silenceLens = stg.SilenceLengths; // [2.0, 2.3, 2.5, 2.7, 3.0, 3.3, 3.7];
 
-		WriteLine("end mp3 splitter!");
+		for(int i = 0; i < silenceLens.Length; i++) {
+			double silenceLen = silenceLens[i];
+
+			string logsNm = $"logs1-{silenceLen}s";
+
+			switch(stg.Type) {
+				case RunnerType.WriteFFMpegSilenceScript: {
+					AlbumToTracksInfo ati = new(stg.Directory) {
+						LogFolderName = logsNm,
+						RemoveRootDirFromScript = false,
+					};
+
+					await ati.RUN(silenceInSecs: silenceLen, runProcess: false);
+
+					string script = ati.Scripts;
+
+					break;
+				}
+				case RunnerType.ConvertFFMpegSilenceLogsToCSVs: {
+					// write csvs from raw results
+					RunParserOnLogsFolder ff = new();
+					ff.RUN(logsDir: stg.Directory + '/' + logsNm);
+					break;
+				}
+				//case RunnerType.WriteSettings: {
+				//	RunnerSettings stgs = new() {
+
+				//	};
+				//}
+			}
+		}
+		WriteLine("!end!");
 	}
-	else {
-		// write csvs from raw results
-		RunParserOnLogsFolder ff = new();
-		ff.RUN(logsDir: dir + '/' + logsNm);
+}
+
+
+public enum RunnerType
+{
+	None = 0,
+	WriteFFMpegSilenceScript = 1,
+	ConvertFFMpegSilenceLogsToCSVs = 2,
+	//WriteSettings = 8,
+}
+
+public class RunnerSettings
+{
+	public RunnerType Type { get; set; }
+
+	public double[] SilenceLengths { get; set; }
+
+	public string Directory { get; set; }
+
+
+	public static RunnerSettings TryParse(string content, string directory)
+	{
+		if(content.IsNulle() || !content.Contains('{'))
+			return null;
+
+		try {
+			RunnerSettings stg = content.DeserializeJson<RunnerSettings>();
+			if(stg.Type == RunnerType.None)
+				return null; // just taking this as sign contents wasn't close to valid
+
+			if(stg.Directory == null)
+				stg.Directory = directory;
+			return stg;
+		}
+		catch {
+			return null;
+		}
 	}
+
 }
